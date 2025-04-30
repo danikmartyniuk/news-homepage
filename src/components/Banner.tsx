c03184dc9e1703acb2d9273173e2c19b02ac052f
Daniil Martyniuk <dan.martyniuk@coral.team>
2025-04-30 02:48:24 +0200

    Added main section layout, font and Banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,7 +1,20 @@
 export default function Banner() {
     return (
-        <div className="flex flex-col items-center justify-center w-full">
-            {/*I'm banner*/}
+        <div className="col-span-2 flex flex-col items-center justify-center gap-8">
+            <img className="" src={require("../assets/images/image-web-3-desktop.jpg")}  alt="Banner image"/>
+            <div className="flex flex-row items-center justify-between">
+                <h1 className="w-1/2 text-[54px] leading-none font-extrabold text-left text-slate-950 h-full flex flex-col items-start justify-center text-pretty pr-4">
+                    The Bright Future of Web 3.0?
+                </h1>
+                <div className="w-1/2 flex flex-col items-start justify-between gap-8 h-full">
+                    <p className="text-slate-600">
+                        We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. But is it really fulfilling its promise?
+                    </p>
+                    <button className="bg-red-500 w-1/2 h-12 text-sm font-extrabold uppercase tracking-[0.2rem] text-slate-950">
+                        Read more
+                    </button>
+                </div>
+            </div>
         </div>
     );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,17 @@
 import Banner from "../components/Banner";
 import Navigation from "../components/Navigation";
+import New from "../components/New";
+import Articles from "../components/Articles";
 
 export default function Home() {
     return (
         <div className="flex-1 py-20 px-36">
             <Navigation />
-            <Banner />
+            <div className="grid grid-cols-3 pt-16">
+                <Banner />
+                <New />
+                <Articles />
+            </div>
         </div>
     );
 }
