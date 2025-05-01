4d98b2d78f0cf5bfa3f8ec3ec150dd2813587d46
Daniil Martyniuk <dan.martyniuk@coral.team>
2025-05-02 01:27:50 +0200

    Added the New widget

---
+++ src/components/New.tsx
@@ -0,0 +1,40 @@
+const newArticles: { title: string, description: string }[] = [
+    {
+        title: 'Hydrogen VS Electric Cars',
+        description: 'Will hydrogen-fueled cars ever catch up to EVs?'
+    },
+    {
+        title: 'The Downsides of AI Artistry',
+        description: 'What are the possible adverse effects of on-demand AI image generation?'
+    },
+    {
+        title: 'Is VC Funding Drying Up?',
+        description: 'Private funding by VC firms is down 50% YOY. We take a look at what that means.'
+    },
+];
+
+export default function New() {
+    return (
+        <div className="col-span-1 flex flex-col items-start justify-between py-8 px-6 gap-8 bg-slate-950">
+            <h2 className="text-5xl font-bold text-orange-300">
+                New
+            </h2>
+            <div className="flex flex-col items-center justify-between gap-8">
+                {
+                    newArticles.map((article, index) => {
+                        return (
+                            <div>
+                                <div key={index} className="flex flex-col items-start justify-between gap-2">
+                                    <a href={`/${index}`}
+                                       className="text-xl font-bold text-stone-50">{article.title}</a>
+                                    <p className="text-sm text-zinc-300">{article.description}</p>
+                                </div>
+                                {index < newArticles.length - 1 && (<hr className="mt-8 w-full border-0 bg-stone-50 h-[0.5px]"/>)}
+                            </div>
+                        );
+                    })
+                }
+            </div>
+        </div>
+    );
+}
